f50b01e8e2b8ccf92ff4c7146c40ed9541ce0626
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-06-25 18:03:42 +0000

    Earth and Stars component completed

--- components/canvas/Stars.tsx
+++ components/canvas/Stars.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+'use client'
+import React, { Suspense, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Points, PointMaterial, Preload } from '@react-three/drei'
+import * as random from 'maath/random/dist/maath-random.cjs'
+import * as Three from 'three'
 
 const Stars = () => {
+  const ref = useRef<Three.Points>(null!);
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 10;
+  })
+
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 }) as Float32Array;
+
   return (
-    <div>Stars</div>
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} stride={3} positions={sphere}>
+        <PointMaterial
+          transparent
+          color='#f272c8'
+          size={0.001}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className='w-full h-full absolute inset-0 z-[-1]'>
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+    </div>
+
+  )
+}
+
+export default StarsCanvas
