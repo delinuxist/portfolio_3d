90ccffdc0e9f91aab2cc03d5a60bb478787619b2
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-06-25 18:08:03 +0000

    config files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["three"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+        pathname: "/api/portraits/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
